a24b95208e069107807e73afbbc89153a5c5f5ab
CSnowden1 <christopher.snowden@protonmail.com>
2023-04-16 12:17:50 -0400

    Began to add the js functionality to load another folder when a folder is clicked

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ dots.forEach((dot, index) => {
     panels.style.transform = `translateX(-${index * 100}%)`;
   });
 });
+
+
+let folder = document.querySelectorAll("folder");
+console.log(folder);
+folder.forEach((bin) => {
+  bin.addEventListener('clicked', () => {
+    console.log("folder clicked")
+    let contentBox = createElement("div");
+    //add class for content box
+    let container = createElement('div');
+    let title = createElement("h4");
+    let gridBox = createElement("div");
+    let boxGrid = createElement("div")
+  })
+})
