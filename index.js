8c7950a0cc2faa750e01214a1f44665d1471e5d1
CSnowden1 <christopher.snowden@protonmail.com>
2023-04-19 13:07:17 -0400

    added chomre api to render bookmarks

--- index.js
+++ index.js
@@ -1,6 +1,55 @@
 //let bookmarkGrid = document.getElementById("bookmark-grid");
 
+let bookmarkGrid = document.getElementById("bookmark-grid");
+let containerElement = document.createElement("div").classList.add("container");
+let topBoxElement = document.createElement("div").classList.add("top-box");
+let contentBoxElement = document.createElement("div").classList.add("content-box");
+let gridBoxElement = document.createElement("div").classList.add("grid-box");
+let itemElement = document.createElement("div").classList.add("item");
+let folderElement = document.createElement("div").document.createElement("i").classList.add("fas fa-folder folder");
+folderElement.id = "folder";
+let folderNameElement = document.createElement("div").document.createElement("p");
 
+
+let gridFolderItem = containerElement.append(topBoxElement).append(contentBoxElement).append(gridBoxElement).append(itemElement).append(itemElement).append(folderElement).append(folderNameElement)
+
+
+let generateBookmarkDirectory = (bookmarks, parent) => {
+  let ul = document.createElement("ul");
+  ul.classList.add('menu', 'vertical');
+  parent.appendChild(ul); // Append the submenu to the parent <li> element
+
+  bookmarks.forEach(function(bookmark) {
+    if (bookmark.hasOwnProperty('children')) {
+      console.log('This is a Folder');
+      bookmarkGrid.append(gridFolderItem)
+    } else {
+      bookmarkGrid.append(folderElement)
+    }
+    let li = document.createElement("li");
+    let a = document.createElement("a");
+    a.textContent = bookmark.title;
+    if (bookmark.children) {
+      li.classList.add('has-submenu');
+      a.setAttribute('data-toggle', 'submenu');
+      a.setAttribute('aria-haspopup', 'true');
+      a.setAttribute('aria-expanded', 'false');
+      let submenu = generateBookmarkDirectory(bookmark.children, li);
+      li.appendChild(submenu);
+    } else {
+      a.href = bookmark.url;
+    }
+    li.appendChild(a);
+    ul.appendChild(li);
+  });
+
+  return ul; 
+}
+
+chrome.bookmarks.getTree(function(bookmarkTreeNodes) {
+  let bookmarks = bookmarkTreeNodes;
+  generateBookmarkDirectory(bookmarks, bookmarkGrid);
+});
 
 
 const dots = document.querySelectorAll('.dot');
